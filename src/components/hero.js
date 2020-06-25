9d40786bd1ddf2fe485be888f5343ea32a9520dd
Akash Kumar <aksh.iem22@gmail.com>
2020-06-25 11:59:11 +0530

    saving changes

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,7 +5,7 @@ const Hero = () => (
     <>
         <div className="hero">
             <div className="hero-textContainer">
-                <span>Hi my name is</span>
+                <span className="subText">Hi my name is</span>
                 <h1>Akash Kumar</h1>
                 <span>I am interested in</span>
                 <span>Front-End Development</span>
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+// import Header from "./header"
 // import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -18,12 +18,10 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
       <div
         style={{
           margin: `0 250px`,
-          maxWidth: `100vw`,
-          minHeight: `80vh`,
           padding: `0 1.0875rem 1.45rem`,
           border: `1px solid red`
         }}
